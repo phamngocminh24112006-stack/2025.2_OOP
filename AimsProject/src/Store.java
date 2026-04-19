55a0e2a9decddbee57efeb0731e930eb72f3d9a7
Phạm Ngọc Minh <phamngocminh24112006@gmail.com>
2026-04-19 15:55:41 +0700

    implement store class

---
+++ AimsProject/src/Store.java
@@ -0,0 +1,38 @@
+public class Store {
+	public static final int MAX_ITEMS_IN_STORE = 100;
+	private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc [MAX_ITEMS_IN_STORE];
+	private int qtyInStore = 0;
+	
+	public void addDVD (DigitalVideoDisc disc) {
+		if (qtyInStore < MAX_ITEMS_IN_STORE) {
+            itemsInStore[qtyInStore] = disc;
+            qtyInStore++;
+            System.out.println("The DVD has been added to store");
+        } else {
+            System.out.println("The store is full");
+        }
+	}
+	public void removeDVD(DigitalVideoDisc disc) {
+        boolean found = false;
+
+        for (int i = 0; i < qtyInStore; i++) {
+            if (itemsInStore[i] == disc) {
+                found = true;
+
+                for (int j = i; j < qtyInStore - 1; j++) {
+                    itemsInStore[j] = itemsInStore[j + 1];
+                }
+
+                itemsInStore[qtyInStore - 1] = null;
+                qtyInStore--;
+
+                System.out.println("The DVD has been removed from store");
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("The DVD was not found in store");
+        }
+    }
+}
