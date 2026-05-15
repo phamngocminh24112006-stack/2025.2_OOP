f71f523ed1d5c4a64aae212101e55859d518ef7b
Phạm Ngọc Minh <phamngocminh24112006@gmail.com>
2026-05-15 17:04:01 +0700

    add playable interface

--- AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ AimsProject/src/hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -1,5 +1,5 @@
 package hust.soict.dsai.aims.media;
-public class DigitalVideoDisc extends Disc {
+public class DigitalVideoDisc extends Disc implements Playable{
 	private static int nbDigitalVideoDiscs = 0;
 
 	
@@ -35,6 +35,12 @@ public class DigitalVideoDisc extends Disc {
 	public String toString() {
 		return "DVD - " + title + " - " + category + " - "
 				+ director + " - " + length +": " + cost + " $";
+	}
+	@Override
+	public void play() {
+		System.out.println ("Playing DVD: "+ this.getTitle());
+		System.out.println ("DVD length: " + this.getLength());
+		
 	}
 	
 }
